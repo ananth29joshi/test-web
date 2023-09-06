f90d12ddd1a8f79583419ee09995480297f19c19
ananth29joshi <143822988+ananth29joshi@users.noreply.github.com>
2023-09-06 22:53:30 +0530

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,26 +1,25 @@
-import { decorateIcons, getLanguagePath } from '../../scripts/lib-franklin.js';
-import { getNavPath } from '../header/header.js';
+import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
 
 /**
  * loads and decorates the footer
- * @param {Element} block The header block element
+ * @param {Element} block The footer block element
  */
-
 export default async function decorate(block) {
+  const cfg = readBlockConfig(block);
   block.textContent = '';
 
-  const resp = await fetch(`${getLanguagePath()}${getNavPath('footer')}.plain.html`);
-  const html = await resp.text();
-  const footer = document.createElement('div');
-  footer.innerHTML = html;
+  // fetch footer content
+  const footerPath = cfg.footer || '/footer';
+  const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
 
-  // add link title for all icon only links
-  footer.querySelectorAll('a > span.icon').forEach((icon) => {
-    const a = icon.parentElement;
-    const iconName = icon.classList[1].substring(5, icon.classList[1].length);
-    a.title = a.title || iconName;
-  });
+  if (resp.ok) {
+    const html = await resp.text();
 
-  await decorateIcons(footer);
-  block.append(footer);
+    // decorate footer DOM
+    const footer = document.createElement('div');
+    footer.innerHTML = html;
+
+    decorateIcons(footer);
+    block.append(footer);
+  }
 }
